c678ed010ed029fb59850d4d8b3ecd83e53625f1
Morgan Prior <morgan@amaras-tech.co.uk>
2010-09-24 00:54:31 +0100

    Example3 changed to exercis row and column map functions

--- example/example3.rb
+++ example/example3.rb
@@ -2,42 +2,63 @@ require '../lib/tabular'
 require 'pp'
 
 
-table = Tabular::Table.new([[0,0,0],[4,5,6],[7,8,9]])
+table = Tabular::Table.new([["a","b","c"],[1,2,3],[4,5,6],[7,8,9]], true)
 pp table
 
-table.save_row( 0, [1,2,3] )
-pp table
-
-
-
-table.name_column(0, "first")
-table.name_column(1, "second")
-table.name_column(2, "third")
-
+puts "\nmap! on table"
 table.map! do |row|
    row[2] = row[2] * 2
    row
 end
 pp table
 
-b = table.fetch( 0 ).map! do |item|
-   item = item * 2
+puts "\nmap_row 0 item**2"
+row = table.map_row( 0 ) do |item|
+   item**2
 end
-table.save_row( 0, b)
-
+pp row
 pp table
 
-pp table.fetch_column( "first" )
-pp table.fetch_column( 1 )
-
-cola = table.fetch_column( "third" ).map! do |item|
-   item = item * 2
+puts "\nmap_row! 0 item**2"
+table.map_row!( 0 ) do |item|
+   item**2
 end
-table.save_column( "third", cola )
-
-puts
-puts "Ran map on third column"
+pp row
 pp table
 
+puts "\nmap_column 2 item+1"
+d = table.map_column( "c" ) do |item|
+   item+1
+end
+pp d
+pp table
+
+puts "\nmap_column! 2 item+1"
+d = table.map_column!( "c" ) do |item|
+   item+1
+end
+pp d
+pp table
+
+
+#b = table.fetch( 0 ).map! do |item|
+#   item = item * 2
+#end
+#table.save_row( 0, b)
+#
+#pp table
+#
+#pp table.column_at( "first" )
+#pp table.column_at( 1 )
+#
+#cola = table.fetch_column( "third" ).map! do |item|
+#   item = item * 2
+#end
+#table.save_column( "third", cola )
+#
+#puts
+#puts "Ran map on third column"
+#pp table
+#
 
 
