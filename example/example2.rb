f7a3135b9d63ffe74e870b1d5fc7ab4c42d97d3a
Morgan Prior <morgan@amaras-tech.co.uk>
2010-09-24 00:54:07 +0100

    Example2 changed to exercise all standard column functions

--- example/example2.rb
+++ example/example2.rb
@@ -1,27 +1,41 @@
 require '../lib/tabular'
 require 'pp'
 
+puts "Example of Column Methods, with initialized table"
 
-table = Tabular::Table.new([[1,2,3],[4,5,6],[7,8,9]])
-
+table = Tabular::Table.new([[1,2,0],[4,5,0],[7,8,0]])
 pp table
 
-
+puts "\nNaming columns"
 table.name_column(0, "first")
 table.name_column(1, "second")
 table.name_column(2, "third")
+pp table
 
-puts table.fetch(1,"second")
+puts "\nSave column 2 [3,6,9]"
+table.save_column(2, [3,6,9] )
+pp table
 
-pp table.fetch_column( 0 )
-pp table.fetch_column( "third" )
+puts "Retrieve data via named fetch"
+pp table.fetch(1,"second")
 
+puts "\nRetrieve data via column_at, with integer index then named"
+pp table.column_at( 0 )
+pp table.column_at( "third" )
+
+puts "\nAppend Column"
 table.append_column( [5,7,9] )
 pp table
 
+puts "\nInsert column"
 table.insert_column( 0, [0,3,6] )
 pp table
 
+puts "\nDelete named column"
 table.delete_column( "0" )
+pp table
+
+puts "\nDelete integer index column"
+table.delete_column( 0 )
 pp table
 
